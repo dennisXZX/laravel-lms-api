7b996e2575fdcd7a6e06c2ef1091d02dfbff3ddf
Dennis Xiao <dennisboys@gmail.com>
2017-11-26 09:26:03 +1100

    fixed CORS issue

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
